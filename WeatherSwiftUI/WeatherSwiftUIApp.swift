db7f3aa4f81c01d4962948dfe7ec7a75355a3796
Diana Nuansengsy <diana.nuansengsi@mail.ru>
2021-11-01 07:06:07 +0300

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Диана Нуансенгси on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
